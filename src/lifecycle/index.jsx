aa002ca63d6285ba42319d7d2b186f402ffd91da
yulianto <jooliantoo@gmail.com>
2024-05-14 09:30:26 +0700

    update code lifecycle

--- src/lifecycle/index.jsx
+++ src/lifecycle/index.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import LifecycleClass from "./materi/class";
-import LifecycleFunctional from "./materi/function";
 import LifecycleClass2 from "./materi/sample";
 
 export default function Lifecycle() {
   return (
     <div>
       <LifecycleClass />
-      <LifecycleFunctional />
       <LifecycleClass2 />
     </div>
   );
